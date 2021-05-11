5201c2cc9bd0c05b379a7f5d3cca7c2f0d99b52b
77Z <r2d2fan20@outlook.com>
2021-05-11 16:33:17 +0000

    Buttons

---
+++ src/shared/components/Button.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let raiseOnHover = false;
+</script>
+
+<button on:click|self><slot /></button>
+
+{#if raiseOnHover}
+	<style>
+		button:hover {
+			transform: translateY(-5px);
+		}
+	</style>
+{/if}
+
+<style>
+	button {
+		font-size: 1rem;
+		padding: 10px 25px;
+		box-sizing: border-box;
+		border-radius: 5px;
+		cursor: pointer;
+		border: none;
+		background: #ff4500;
+		color: #fff;
+	}
+</style>
